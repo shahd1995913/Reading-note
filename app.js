e726fcbfd1df4546c9f27678bce02e92520d24ee
shahd1995913 <shahdalkhatib95@gmail.com>
2021-06-20 15:05:46 +0300

    update4

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
-function name(){
+function insertyourname(){
     let val =prompt('Insert youre name plase')
     alert (val)
    
     }
+    insertyourname()
+
+    function image(){
+        var val =prompt('how many image for progragmimg Languages you want to see')
+        
+        for(i=0;i<val;i++)
+        {
+          document.write("<img src='https://www.simplilearn.com/ice9/free_resources_article_thumb/Best-Programming-Languages-to-Start-Learning-Today.jpg' ,width='200px', hight='300';>")
+        } 
+        }
+        
+        image()
 
    
